954daa7986f6ebd2cb54cf78f56cea125c378908
Parv-gugnani <9765.parv@gmail.com>
2023-12-04 19:00:35 +0530

    completed

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 "use client";
-
 import styles from "./page.module.scss";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import useMousePosition from "./utils/useMousePosition";
 
+//
 export default function Home() {
   const [isHovered, setIsHovered] = useState(false);
   const { x, y } = useMousePosition();
@@ -18,7 +18,7 @@ export default function Home() {
           WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
           WebkitMaskSize: `${size}px`,
         }}
-        transition={{ type: "tween", ease: "blackOut", duration: 0.5 }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
       >
         <p
           onMouseEnter={() => {
@@ -28,7 +28,7 @@ export default function Home() {
             setIsHovered(false);
           }}
         >
-          Random text asoihfiosdnfklsdflkjsdfiosnifognsdilfilskjli Lollll
+          Web Developer
         </p>
       </motion.div>
 
--- app/utils/useMousePosition.js
+++ app/utils/useMousePosition.js
@@ -2,7 +2,19 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const useMousePosition = () => {
-  return <div>useMousePosition</div>;
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+  const updateMousePosition = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, []);
+
+  return mousePosition;
 };
 
 export default useMousePosition;
